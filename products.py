1779c423bd3c2fc260b108ffc9384dd306feb509
yaoying228 <yaoying228@gmail.com>
2021-12-12 16:39:18 +0700

    add write function

--- products.py
+++ products.py
@@ -12,3 +12,8 @@ print(products)
 
 for p in products:
     print(p[0],'的價格是:',p[1])
+
+with open('products.csv' , 'w') as f :
+    for p in products:
+	    f.write(p[0] + ','+ p[1] +'\n')
+f.write()
