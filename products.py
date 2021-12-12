6ef6041b985d02e5277ad47c0d2ab140296c23ee
yaoying228 <yaoying228@gmail.com>
2021-12-12 17:07:41 +0700

    add header and encoding

--- products.py
+++ products.py
@@ -14,6 +14,7 @@ print(products)
 for p in products:
     print(p[0],'的價格是:',p[1])
 
-with open('products.csv' , 'w') as f :
+with open('products.csv' , 'w' , encoding = 'utf-8') as f :
+    f.write('商品 , 價格\n')
     for p in products:
-	    f.write(p[0] + ','+ str(p[1]) +'\n')      #No.7 price 轉換成整數了,所以No.19 要變為
+        f.write(p[0] + ','+ str(p[1]) +'\n')      #No.7 price 轉換成整數了,所以No.19 要變為
