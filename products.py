d8f961700db97cf60a3083beedd727e2ed85c47e
yaoying228 <yaoying228@gmail.com>
2021-12-13 20:58:38 +0700

    add re-factor

--- products.py
+++ products.py
@@ -1,38 +1,54 @@
 import os     # operating system
 
-products = []
-if os.path.isfile('products.csv'):
-    print('找到檔案了')
-    with open('products.csv' , 'r' ) as f :
+def read_file(filename):
+    products = []
+    with open(filename, 'r' ) as f :
         for line in f :
             if '商品 , 價格' in line :
                 continue                         #跳到下一回的意思
-        name , price = line.strip().split(',')
-        products.append([name , price])
-    print(products)
-        
-else:
-    print('沒找到檔案,可能遺失了..')
-
+            name , price = line.strip().split(',')
+            products.append([name , price])
+        return products
+      
 #輸入購買紀錄
-while True:
-    name = input('請輸入商品名稱:')
-    if name =='quit':
-        break
-    price = input('商品價格:')
-    price =int(price)
-    p = []
-    p.append(name)                   
-    p.append(price)
-    products.append(p)
-print(products)
+
+def use_input(products):
+    while True:
+        name = input('請輸入商品名稱:')
+        if name =='q':
+            break
+        price = input('商品價格:')
+        price =int(price)
+        p = []
+        p.append(name)                   
+        p.append(price)
+        products.append(p)
+    print(products)
+    return products
 
 #印出購買紀錄
-for p in products:
-    print(p[0],'的價格是:',p[1])
+
+def print_products(products):
+    for p in products:
+        print(p[0],'的價格是:',p[1])
 
 #寫入檔案
-with open('products.csv' , 'w') as f :
-    f.write('商品 , 價格\n')
-    for p in products:
-        f.write(p[0] + ','+ str(p[1]) +'\n')      # price 轉換成整數了,所以要變為strip
+
+def write_file(filename,products):
+    with open(filename , 'w' ) as f :
+        f.write('商品 , 價格\n')
+        for p in products:
+            f.write(p[0] + ','+ str(p[1]) +'\n')      # price 轉換成整數了,所以要變為strip
+
+def main():
+    filename= 'products.csv'
+    if os.path.isfile(filename):
+        print('太棒了!找到檔案囉')
+        products = read_file(filename)
+    else:
+        print('沒找到檔案,可能遺失了..')
+    products = use_input(products)
+    print_products(products)
+    write_file('products.csv',products)
+
+main()
